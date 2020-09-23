761fb7b8d5bf767953af70a8ef8d8fd74552f925
ilkim111 <63088486+ilkim111@users.noreply.github.com>
2020-09-22 21:23:22 -0400

    0922 Change

--- designs.js
+++ designs.js
@@ -1,45 +1,44 @@
 function makeCanvas(height, width) {
-// Your code goes here!
-    const table = document.getElementById("pixelCanvas");
-    let canvas  = '';
+  // Your code goes here!
+  const table = document.getElementById("pixelCanvas");
+  let canvas = "";
 
-// table loop
-for (let i = 0; i < height; i++) {
-    canvas += '<tr class="row"'+ i +'>';
+  // table loop
+  for (let i = 0; i < height; i++) {
+    canvas += '<tr class="row"' + i + ">";
 
     for (let j = 0; j < width; j++) {
-        canvas += '<td class = "cell"'+j + '></td>';
+      canvas += '<td class = "cell"' + j + "></td>";
     }
-    canvas += '</tr>';
-}
-// create canvas
-table.innerHTML = canvas;
+    canvas += "</tr>";
+  }
+  // create canvas
+  table.innerHTML = canvas;
 
-// add event
-addEvent();
+  // add event
+  addEvent();
 }
 
 // submission function
 function onSubmit() {
-    event.preventDefault();
-    const height = document.getElementById('inputHeight').value;
-    const width = document.getElementById('inputWidth').value;
-    makeCanvas(height, width);
+  event.preventDefault();
+  const height = document.getElementById("inputHeight").value;
+  const width = document.getElementById("inputWidth").value;
+  makeCanvas(height, width);
 }
 
+// Add event lister to cells
 function addEvent() {
-    const colorPicker = document.getElementById("colorPicker");
-    const cells = document.getElementsByClassName('cell');
-    for (let i = 0; i < cells.length; i++) {
-        cells[i].addEventListener("click", function (event) {
-            let clickedCell = event.target;
-            clickedCell.style.backgroundColor = colorPicker.value;
-        });
-    }
+  const colorPicker = document.getElementById("colorPicker");
+  const cells = document.getElementsByClassName("cell");
+  for (let i = 0; i < cells.length; i++) {
+    cells[i].addEventListener("click", function (event) {
+      let clickedCell = event.target;
+      clickedCell.style.backgroundColor = colorPicker.value;
+    });
+  }
 }
 
-
-
-document.getElementById('sizePicker').onsubmit = function () {
-    onSubmit();
+document.getElementById("sizePicker").onsubmit = function () {
+  onSubmit();
 };
